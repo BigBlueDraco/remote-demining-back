4422cee11df6c36d7586928009dd345e87fb38b5
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2023-08-06 19:44:52 +0300

    parrentId

--- src/images/schema/image.schema.ts
+++ src/images/schema/image.schema.ts
@@ -3,8 +3,10 @@ import mongoose, { HydratedDocument } from 'mongoose';
 export type UserDocument = HydratedDocument<Image>;
 
 export const ImageSchema = new mongoose.Schema({
+  parentId: { type: String },
   blob: { type: String },
 });
 export interface Image {
+  parentId: string;
   blob: string;
 }
